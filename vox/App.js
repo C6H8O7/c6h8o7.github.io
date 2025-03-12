42ff5d68a2c45944a75888c51194e9341949c635
Marco <marco.lorenzo.64@gmail.com>
2025-03-12 23:33:44 +0100

    build update

--- vox/App.js
+++ vox/App.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {}
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\marco\AppData\Local\Temp\tmpyap6hoh9.js
+// include: C:\Users\marco\AppData\Local\Temp\tmp33qnnhck.js
 Module["expectedDataFileDownloads"] ??= 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -200,29 +200,29 @@ Module["expectedDataFileDownloads"]++;
     }, {
       "filename": "/data/vox.wgsl",
       "start": 883,
-      "end": 12166
+      "end": 15493
     } ],
-    "remote_package_size": 12166
+    "remote_package_size": 15493
   });
 })();
 
-// end include: C:\Users\marco\AppData\Local\Temp\tmpyap6hoh9.js
-// include: C:\Users\marco\AppData\Local\Temp\tmpjv5vg0zy.js
+// end include: C:\Users\marco\AppData\Local\Temp\tmp33qnnhck.js
+// include: C:\Users\marco\AppData\Local\Temp\tmp2fqwsp86.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if (Module["$ww"] || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: C:\Users\marco\AppData\Local\Temp\tmpjv5vg0zy.js
-// include: C:\Users\marco\AppData\Local\Temp\tmpbimb4rv5.js
+// end include: C:\Users\marco\AppData\Local\Temp\tmp2fqwsp86.js
+// include: C:\Users\marco\AppData\Local\Temp\tmpd5sd0lsi.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: C:\Users\marco\AppData\Local\Temp\tmpbimb4rv5.js
+// end include: C:\Users\marco\AppData\Local\Temp\tmpd5sd0lsi.js
 // Sometimes an existing Module object exists with properties
 // meant to overwrite the default module functionality. Here
 // we collect those properties and reapply _after_ we configure
