3c23ff2270b5e42d8f7bc1bc5237b0edfe4c29af
Marco <marco.lorenzo.64@gmail.com>
2025-03-15 13:25:41 +0100

    update build

--- vox/App.js
+++ vox/App.js
@@ -46,7 +46,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\marco\AppData\Local\Temp\tmp_1l0ggt9.js
+// include: C:\Users\marco\AppData\Local\Temp\tmpsrupq0gr.js
 Module["expectedDataFileDownloads"] ??= 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -221,23 +221,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: C:\Users\marco\AppData\Local\Temp\tmp_1l0ggt9.js
-// include: C:\Users\marco\AppData\Local\Temp\tmpnj4v4kaw.js
+// end include: C:\Users\marco\AppData\Local\Temp\tmpsrupq0gr.js
+// include: C:\Users\marco\AppData\Local\Temp\tmp44ps8gl5.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if (Module["$ww"] || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: C:\Users\marco\AppData\Local\Temp\tmpnj4v4kaw.js
-// include: C:\Users\marco\AppData\Local\Temp\tmpc1io1aw2.js
+// end include: C:\Users\marco\AppData\Local\Temp\tmp44ps8gl5.js
+// include: C:\Users\marco\AppData\Local\Temp\tmpr4lko8i6.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: C:\Users\marco\AppData\Local\Temp\tmpc1io1aw2.js
+// end include: C:\Users\marco\AppData\Local\Temp\tmpr4lko8i6.js
 // Sometimes an existing Module object exists with properties
 // meant to overwrite the default module functionality. Here
 // we collect those properties and reapply _after_ we configure
